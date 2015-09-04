e3400b7c8e3b7f1374deaaf65a1573ceec8cbc70
Alexandre Bailon <abailon@baylibre.com>
2015-09-04 11:29:45 +0200

    APBridgeA USB gadget: remove SVC control request
    SVC protocol was using control endpoint to send data from AP to SVC.
    Now, SVC protocol is like other Greybus protocols. It use bulk in and
    out endpoints. Remove old SVC protocol code.
    
    Testing Done: Greybus is working
    Bug Number: SW-795
    Review URL:
    Reviewed by:
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- nuttx/configs/ara/bridge/src/apbridgea_gadget.c
+++ nuttx/configs/ara/bridge/src/apbridgea_gadget.c
@@ -152,7 +152,6 @@
 
 /* Vender specific control requests *******************************************/
 
-#define APBRIDGE_RWREQUEST_SVC          (0x01)
 #define APBRIDGE_RWREQUEST_LOG          (0x02)
 #define APBRIDGE_RWREQUEST_EP_MAPPING   (0x03)
 #define APBRIDGE_ROREQUEST_CPORT_COUNT  (0x04)
@@ -227,7 +226,6 @@ struct cport_to_ep {
 
 enum ctrlreq_state {
     USB_REQ,
-    GREYBUS_SVC_REQ,
     GREYBUS_LOG,
     GREYBUS_EP_MAPPING,
 };
@@ -797,11 +795,9 @@ static void usbclass_ep0incomplete(struct usbdev_ep_s *ep,
                                    struct usbdev_req_s *req)
 {
     struct apbridge_dev_s *priv;
-    struct apbridge_usb_driver *drv;
     int *req_priv;
 
     priv = (struct apbridge_dev_s *) ep->priv;
-    drv = priv->driver;
 
     if (req->result || req->xfrd != req->len) {
         usbtrace(TRACE_CLSERROR(USBSER_TRACEERR_REQRESULT),
@@ -810,8 +806,6 @@ static void usbclass_ep0incomplete(struct usbdev_ep_s *ep,
 
     req_priv = (int *)request_get_priv(req);
     if (req_priv) {
-        if (*req_priv == GREYBUS_SVC_REQ)
-            drv->usb_to_svc(NULL, req->buf, req->len);
         if (*req_priv == GREYBUS_EP_MAPPING)
             map_cport_to_ep(priv, (struct cport_to_ep *)req->buf);
         kmm_free(req_priv);
@@ -1318,15 +1312,7 @@ static int usbclass_setup(struct usbdevclass_driver_s *driver,
         {
             if ((ctrl->type & USB_REQ_RECIPIENT_MASK) ==
                 USB_REQ_RECIPIENT_INTERFACE) {
-                if (ctrl->req == APBRIDGE_RWREQUEST_SVC) {
-                    if ((ctrl->type & USB_DIR_IN) != 0) {
-                        *(uint32_t *) req->buf = 0xdeadbeef;
-                        ret = 4;
-                    } else {
-                        *req_priv = GREYBUS_SVC_REQ;
-                        ret = len;
-                    }
-                } else if (ctrl->req == APBRIDGE_RWREQUEST_LOG) {
+                if (ctrl->req == APBRIDGE_RWREQUEST_LOG) {
                     if ((ctrl->type & USB_DIR_IN) == 0) {
                     } else {
 #if defined(CONFIG_APB_USB_LOG)
