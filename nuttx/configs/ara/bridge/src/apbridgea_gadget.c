a6032d30a2859ad53e0cc2afc5c076da61e992ad
Alexandre Bailon <abailon@baylibre.com>
2016-03-07 14:19:25 +0100

    SW-3519: USB gadget: Give back the request in the case of UniPro error
    If for any reason we got an error before the end of UniPro transfer,
    we need to give back the USB request because UniPro driver will not do it.
    
    Testing Done: Partially tested: UniPro doesn't fail
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    
    Change-Id: If6a77766a2fec3797b55da864a61d98060ae535b

--- nuttx/configs/ara/bridge/src/apbridgea_gadget.c
+++ nuttx/configs/ara/bridge/src/apbridgea_gadget.c
@@ -1184,18 +1184,24 @@ static void usbclass_rdcomplete(struct usbdev_ep_s *ep,
         if (data_movement_wd_start(req->buf, priv, bulkout_timeout))
             lowsyslog("Failed to start the watchdog\n");
 
-        drv->usb_to_unipro(priv, cportid, req->buf , req->xfrd);
-        break;
+        if (!drv->usb_to_unipro(priv, cportid, req->buf , req->xfrd))
+            return;
 
     case -ESHUTDOWN:           /* Disconnection */
         usbtrace(TRACE_CLSERROR(USBSER_TRACEERR_RDSHUTDOWN), 0);
-        return;
+        break;
 
     default:                   /* Some other error occurred */
         usbtrace(TRACE_CLSERROR(USBSER_TRACEERR_RDUNEXPECTED),
                  (uint16_t) - req->result);
         break;
     };
+    /*
+     * We got an error before the UniPro transfer completion so
+     * we have to give back the request to USB.
+     */
+    lowsyslog("USB to UniPro transfer failed: %d\n", req->result);
+    EP_SUBMIT(ep, req);
 }
 
 static void usbclass_wrcomplete(struct usbdev_ep_s *ep,
