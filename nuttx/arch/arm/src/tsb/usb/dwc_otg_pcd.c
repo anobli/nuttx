64f98ea08d14aa9c8a547298c38d51110acd7eed
Alexandre Bailon <abailon@baylibre.com>
2016-03-29 12:16:34 +0200

    SW-3518: USB DWC OTG: Add a missing spinlock unlock
    dwc_otg_pcd_ep_dequeue() may exit without to unlock the spinlock
    (if it can't invalidate the request in the ring descriptor).
    Unlock thr spinlock before to exit the function.
    
    Testing Done: Trivial
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    
    Change-Id: I463e97f63e2ed122d5a619b1fb2f415561efa421

--- nuttx/arch/arm/src/tsb/usb/dwc_otg_pcd.c
+++ nuttx/arch/arm/src/tsb/usb/dwc_otg_pcd.c
@@ -3014,6 +3014,7 @@ int dwc_otg_pcd_ep_dequeue(dwc_otg_pcd_t * pcd, void *ep_handle,
 	/* Invalidate the entry in the ring to cause a BNA */
 	if (!ep->dwc_ep.is_in &&
 		invalidate_ring_entry(&ep->dwc_ep, req->dma_desc) < 0) {
+		DWC_SPINUNLOCK_IRQRESTORE(pcd->lock, flags);
 		return -DWC_E_INVALID;
 	}
 	dwc_otg_pcd_dequeue_req(pcd->core_if, ep, req);
